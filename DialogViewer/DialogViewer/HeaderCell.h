cd29a08103ee8b1284b734b3a71847fd561e6e65
923859241@qq.com <923859241@qq.com>
2021-07-06 16:58:59 +0800

    init

---
+++ DialogViewer/DialogViewer/HeaderCell.h
@@ -0,0 +1,17 @@
+//
+//  HeaderCell.h
+//  DialogViewer
+//
+//  Created by chen on 2021/6/21.
+//
+
+#import <UIKit/UIKit.h>
+#import "CollectionViewCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HeaderCell : CollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DialogViewer/DialogViewer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DialogViewer
+//
+//  Created by chen on 2021/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UICollectionViewController<UICollectionViewDelegateFlowLayout>
+
+
+@end
+
